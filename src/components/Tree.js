d1c77f2295df41d786a30001b8fe7e240f9d4cb0
Joshua T Kalis <kalisjoshua@gmail.com>
2018-12-05 11:18:11 -0500

    working

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -11,79 +11,71 @@ const icon = (__html, title, fn) => (
   />
 )
 
-class Parent extends Component {
-  state = {
-    siblings: []
+class Workitem extends Component {
+  constructor({tree = []}) {
+    super()
+
+    this.setState({tree})
   }
   
-  constructor({siblings}) {
-    super()
-    
-    this.state.siblings = siblings || []
-  }
+  add(index, sibling) {
+    const title = prompt("Title")
 
+    if (title) {
+      if (sibling) {
+        this.state.tree.splice(index + 1, 0, {title})
+        this.setState({tree: this.state.tree})
+      } else {
+        const target = this.state.tree[index]
+
+        target.children = target.children || []
+        
+        target.children.push({title})
+        
+        this.setState({}) // force re-render
+      }
+    }
+  }
+  
+  estimate(index) {
+    const e = prompt("Estimate")
+    
+    if (e) {
+      const target = this.state.tree[index]
+
+      target.e = e
+      
+      this.setState({})
+    }
+  }
+  
   remove(index) {
-    this.setState({siblings: [
-      ...this.state.siblings.slice(0, index),
-      ...this.state.siblings.slice(index + 1)
+    this.setState({tree: [
+      ...this.state.tree.slice(0, index),
+      ...this.state.tree.slice(index + 1)
     ]})
   }
-
-  render(props, state) {
+  
+  render() {
+    const isEmpty = !this.state.tree || !this.state.tree.length
 
     return (
       <ul class="worktree">
-        {(state.siblings.length ? state.siblings : [{}])
-          .map((n, i) => <Child parent={this} {...n} index={i} />)}
+        {(isEmpty ? [{}] : this.state.tree).map((node, i) => (
+          <li class="worktree--branch">
+            <div class="worktree--branch__title">
+              {icon("+", "Add Sibling", () => this.add(i, true))}
+              {!isEmpty && icon("&raquo;", "Add Child", () => this.add(i))}
+              <strong>{node.title}</strong>
+              {!isEmpty && icon(`${node.e || "?"}`, "Estimate", () => this.estimate(i))}
+              {!isEmpty && icon("&times;", "Remove Child", () => this.remove(i))}
+            </div>
+            {node.children && <Workitem tree={node.children} />}
+          </li>
+        ))}
       </ul>
-    )
+      )
   }
 }
 
-class Child extends Component {
-  constructor({children, parent, title}) {
-    super()
-
-    this.state.children = children || []
-    this.state.parent = parent
-    this.state.title = title
-  }
-
-  add(sibling) {
-    const title = prompt('Title')
-
-    if (title) {
-      const add = (parent, collection, item) =>
-        parent.setState({[collection]: [...parent.state[collection], {title}]})
-
-      sibling
-        ? add(this.state.parent, 'siblings')
-        : add(this, 'children')
-
-      this.state.parent.forceUpdate()
-    }
-  }
-
-  remove(index) {
-    this.state.parent.remove(index)
-  }
-
-  render(props) {
-    const {index, title} = props
-
-    return (
-      <li class="worktree--branch">
-        <div class="worktree--branch__title">
-          {icon("+", "Add Sibling", () => this.add(true))}
-          {title && icon("&raquo;", "Add Child", () =>  this.add())}
-          <strong contentEditable>{title}</strong>
-          {/* Estimate */}
-          {title && icon("&times;", "Remove Child", () =>  this.remove(index))}
-        </div>
-        {!!this.state.children.length && <Parent siblings={this.state.children} />}
-      </li>
-    )
-  }
-}
-
-export default Parent
+export default Workitem
