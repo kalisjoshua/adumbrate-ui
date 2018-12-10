04fa045a21b0174f2538da0b4d63208aee2d3d23
Joshua T Kalis <kalisjoshua@gmail.com>
2018-12-10 15:29:10 -0500

    save point

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -11,70 +11,115 @@ const icon = (__html, title, fn) => (
   />
 )
 
-function rollup(node, est) {
-  console.log(est, node)
+function add(self, index, sibling) {
+  const title = prompt("Title")
+
+  if (title) {
+    if (sibling) {
+      self.state.tree.splice(index + 1, 0, {title})
+      self.setState({tree: self.state.tree})
+    } else {
+      const target = self.state.tree[index]
+
+      target.children = target.children || []
+      
+      target.children.push({title})
+      
+      self.setState({}) // force re-render
+    }
+  }
+}
+
+function estimate(self, index) {
+  const e = prompt("Estimate")
+  
+  if (e) {
+    const target = self.state.tree[index]
+
+    target.e = e
+    
+    // console.log(target)
+    rollup(self, e)
+    self.setState({})
+  }
+}
+
+function remove(self, index) {
+  let siblings = self.state.tree
+
+  siblings = [
+    ...siblings.slice(0, index),
+    ...siblings.slice(index + 1)
+  ]
+
+  if (siblings.length) {
+    self.setState({tree: siblings})
+  } else if (!self.state.parent) {
+    self.setState({tree: []})
+  } else {
+    const parentSiblings = self.state.parent.state.parent.state.tree
+
+    delete parentSiblings[self.state.index].children
+
+    self.state.parent.setState({tree: parentSiblings})
+  }
+}
+
+function rollup(element) {
+  const sum = (ar) => ar
+    .reduce((a, b) => a + +(b.e || 0), 0)
+
+  let node = element
+
+  while (node) {
+    if (!node.state.tree) {
+      // console.log(sum(node.state.tree), node.state)
+      console.log(sum(node.state.node.children))
+    }
+
+    node = node.state.parent
+  }
   // throw new Error("Not Implemented Yet!")
 }
 
-class Workitem extends Component {
-  constructor({index, parent, tree = []}) {
+class WorkitemElement extends Component {
+  constructor({node, parent}) {
     super()
 
+    this.setState({node, parent})
+  }
+  
+  render({i, isEmpty, node, parent}) {
+    return (
+      <li class="worktree--branch">
+        <div class="worktree--branch__title">
+          {icon("+", "Add Sibling", () => add(parent, i, true))}
+          {!isEmpty && icon("&raquo;", "Add Child", () => add(parent, i))}
+          <strong>{node.title}</strong>
+          {!isEmpty && icon(`${node.e || "?"}`, "Estimate", () => estimate(parent, i))}
+          {!isEmpty && icon("&times;", "Remove Child", () => remove(parent, i))}
+        </div>
+        {node.children && <WorkitemTree index={i} parent={this} tree={node.children} />}
+      </li>
+    )
+  }
+}
+
+class WorkitemTree extends Component {
+  constructor({index, parent, tree = []}) {
+    super()
+    
+    tree.forEach((parent, parentIndex) => {
+      (parent.children || [])
+        .forEach((child, childIndex) => {
+          child.parent = this
+          child.parentNode = parent
+        })
+    
+      // parent.element = this
+    })
+
     this.setState({index, parent, tree})
-  }
-  
-  add(index, sibling) {
-    const title = prompt("Title")
-
-    if (title) {
-      if (sibling) {
-        this.state.tree.splice(index + 1, 0, {title})
-        this.setState({tree: this.state.tree})
-      } else {
-        const target = this.state.tree[index]
-
-        target.children = target.children || []
-        
-        target.children.push({title})
-        
-        this.setState({}) // force re-render
-      }
-    }
-  }
-  
-  estimate(index) {
-    const e = prompt("Estimate")
-    
-    if (e) {
-      const target = this.state.tree[index]
-
-      target.e = e
-      
-      console.log(target)
-      // rollup(this.state.parent, e)
-      this.setState({})
-    }
-  }
-  
-  remove(index) {
-    let siblings = this.state.tree
-    
-    siblings = [
-      ...siblings.slice(0, index),
-      ...siblings.slice(index + 1)
-    ]
-
-    if (siblings.length) {
-      this.setState({tree: siblings})
-    } else if (!this.state.parent) {
-      this.setState({tree: []})
-    } else {
-      const parentSiblings = this.state.parent.state.tree
-
-      delete parentSiblings[this.state.index].children
-
-      this.state.parent.setState({tree: parentSiblings})
-    }
   }
   
   render() {
@@ -82,21 +127,13 @@ class Workitem extends Component {
 
     return (
       <ul class="worktree">
-        {(isEmpty ? [{}] : this.state.tree).map((node, i) => (
-          <li class="worktree--branch">
-            <div class="worktree--branch__title">
-              {icon("+", "Add Sibling", () => this.add(i, true))}
-              {!isEmpty && icon("&raquo;", "Add Child", () => this.add(i))}
-              <strong>{node.title}</strong>
-              {!isEmpty && icon(`${node.e || "?"}`, "Estimate", () => this.estimate(i))}
-              {!isEmpty && icon("&times;", "Remove Child", () => this.remove(i))}
-            </div>
-            {node.children && <Workitem index={i} parent={this} tree={node.children} />}
-          </li>
-        ))}
+        {(isEmpty ? [{}] : this.state.tree)
+          .map((node, i) => (
+            <WorkitemElement isEmpty={isEmpty} i={i} node={node} parent={this}/>
+          ))}
       </ul>
       )
   }
 }
 
-export default Workitem
+export default WorkitemTree
